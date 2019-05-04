e5fef71572d3092b49b3f2a6bb334f795b923f3f
Bart Veneman <bartveneman@users.noreply.github.com>
2019-05-04 21:20:43 +0200

    moar stats

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const got = require("got");
 const analyzeCss = require("@projectwallace/css-analyzer");
 const { urlencoded } = require("body-parser");
 const formatFilesize = require("pretty-bytes");
+const normalizeUrl = require("normalize-url");
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
@@ -19,11 +20,15 @@ app.use((req, res, next) => {
 });
 
 app.get("/", async (req, res) => {
-  if (req.query.url) {
-    const response = await got(`https://extract-css.now.sh/${req.query.url}`);
+  const url = req.query.url
+    ? normalizeUrl(req.query.url, { stripWWW: false })
+    : undefined;
+
+  if (url) {
+    const response = await got(`https://extract-css.now.sh/${url}`);
     const css = response.body;
     const stats = await analyzeCss(css);
-    return res.render("stats", { stats, css, url: req.query.url });
+    return res.render("stats", { stats, css, url });
   }
   return res.render("index");
 });
