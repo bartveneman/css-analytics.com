a4e688cefea40f6765891455781ff49bfe2bf9be
Bart Veneman <bartveneman@users.noreply.github.com>
2019-05-28 23:06:34 +0200

    add box-shadows, compressed filesizes and color bar

--- index.js
+++ index.js
@@ -3,19 +3,29 @@ const path = require("path");
 const got = require("got");
 const analyzeCss = require("@projectwallace/css-analyzer");
 const { urlencoded } = require("body-parser");
-const formatFilesize = require("pretty-bytes");
+const formatFileSize = require("pretty-bytes");
 const normalizeUrl = require("normalize-url");
+const tinycolor = require("tinycolor2");
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use((req, res, next) => {
-  res.locals.formatFilesize = formatFilesize;
+  res.locals.formatFileSize = formatFileSize;
   res.locals.formatNumber = number =>
     Number.isInteger(number)
       ? new Intl.NumberFormat().format(number)
       : parseFloat(number).toFixed(3);
+  res.locals.formatSvgColor = color => {
+    const parsed = tinycolor(color);
+
+    if (parsed.getFormat() === "hex") {
+      return parsed.toHexString();
+    }
+
+    return color;
+  };
   next();
 });
 
