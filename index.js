d20fea6581769fcaa3d62af01ad72ba3730daaad
Bart Veneman <bartveneman@users.noreply.github.com>
2019-07-26 21:14:16 +0200

    strip url protocol, as the protocol makes the app crash

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 
 app.get("*", async (req, res) => {
   const url = req.query.url
-    ? normalizeUrl(req.query.url, { stripWWW: false })
+    ? normalizeUrl(req.query.url, { stripWWW: false, stripProtocol: true })
     : undefined;
 
   if (url) {
